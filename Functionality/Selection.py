3143b6ec1269769201959dc22134690169df495f
Dominik-Pawelec <dominikpawelec@gmail.com>
2024-01-31 23:24:08 +0100

    added selection.py file allowing access to selection file, new functions in Video_Manipualtion

---
+++ Functionality/Selection.py
@@ -0,0 +1,50 @@
+
+def clearNumberStr(text):  
+    times = text.split(":")
+    if len(times) == 2:
+        time_sec = int(times[0])*60+int(times[1])
+    elif len(times) == 3:
+        time_sec = int(times[0])*3600+int(times[1])*60+int(times[2])
+    else:
+        print(f"Something went wrong with clearNumberStr({text}). function aborted.")
+        return
+    return time_sec
+
+
+class timeStamps:
+    time_raw = []
+    time_list = []
+    def __init__(self,data_file):
+        with open(data_file, encoding = "utf-8") as data:
+            for line in data:
+                division = line.find("-")
+                nr_1 = "".join([line[x] for x in range(division)])
+                nr_1 = nr_1.replace(" ","")
+                nr_2 = "".join([line[division + x] for x in range(1,len(line)-division)])
+                nr_2 =nr_2.replace(" ","").replace("\n","")
+
+                self.time_raw.append([nr_1,nr_2]) #nit necessaty but maybe we'll be used
+                self.time_list.append([clearNumberStr(nr_1),clearNumberStr(nr_2)])
+
+                
+            
+    def getTime(self,nr):
+        return self.time_list[nr]
+    
+    def getTimeList(self):
+        return self.time_list
+            
+    
+        
+
+
+
+if __name__ == "__main__":
+    
+    a = timeStamps("Test_Folder/test.txt")
+
+    
+    print(a.getTimeList())
+    
+    
+    print("\nFinished testing Selection.py")
--- Functionality/video_manipulations.py
+++ Functionality/video_manipulations.py
@@ -1,21 +1,52 @@
+import Selection as Sel
+
 import moviepy.editor as mp
 import os
+
 
 def mergeClips(path):
     file_list = os.listdir(path)
     videos = [mp.VideoFileClip(path+file) for file in file_list]
     if len(videos) != 0:
         return mp.concatenate(videos)
-    else:
-        return None
+    return None
 
-def exportClip(clip:mp.VideoFileClip, export_path, file_name):
+def exportClip(clip:mp.VideoFileClip, export_path, file_name,render_res = None):
     path_file = export_path + file_name
+    if os.path.exists(path_file):
+        print(f"path {path_file} already exists, change name of the file to proceed")
+        return #TO DO: auto change name
+    if render_res != None:
+        clip = clip.resize(render_res)
     clip.write_videofile(path_file)
     
+def cutVideo(clip:mp.VideoFileClip, begin_t, end_t, precise_t = False):
+    if not precise_t:
+        return clip.subclip(begin_t,end_t+1)
+    else:
+        return clip.subclip(begin_t,end_t)
 
+def cutTimeStampClips(clip:mp.VideoFileClip, timestamps : Sel.timeStamps, precise_t = False):
+    list_of_vid = []
+    for times in timestamps.time_list:
+        list_of_vid.append(cutVideo(clip,times[0],times[1]))
+    
+    #security checks: what if list gives thing out of order
+    return list_of_vid
+        
+
+
+
+
+
+#=====TESTING=====
 if __name__ == "__main__":
+    
+    timestamps = Sel.timeStamps("Test_Folder/test.txt")
+    
     print("Testing: video_manipulations.py ;")
-    temp = mergeClips("Test_Folder/")
-    exportClip(temp,"Test_Folder/","test.mp4")
-    # path is "nice"
+    temp = cutVideo(mergeClips("Test_Folder/"),35,40)
+    exportClip(temp,"Test_Folder/","h.mp4",(1000,800))
+    
+    
+    print("Finished testing Video_Manipulations.py")
