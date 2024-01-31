c04764e5e98b2964b11f0d01a49de2e0608f429a
Dominik-Pawelec <dominikpawelec@gmail.com>
2024-02-01 00:08:59 +0100

    implemented exporting list of videos to new folder

--- Functionality/video_manipulations.py
+++ Functionality/video_manipulations.py
@@ -19,6 +19,16 @@ def exportClip(clip:mp.VideoFileClip, export_path, file_name,render_res = None):
     if render_res != None:
         clip = clip.resize(render_res)
     clip.write_videofile(path_file)
+    # if exists, return "hey it worked"
+    
+def exportClipList(clip_list, export_path, folder_name):
+    #print(os.getcwd()) //dont delete, useful reminder later that hose are indirect paths not direct
+    os.mkdir(export_path + folder_name)
+    
+    for i in range(len(clip_list)):
+        name = f"file_{i}.mp4"
+        exportClip(clip_list[i],f"{export_path}{folder_name}/",name)
+    
     
 def cutVideo(clip:mp.VideoFileClip, begin_t, end_t, precise_t = False):
     if not precise_t:
@@ -31,9 +41,11 @@ def cutTimeStampClips(clip:mp.VideoFileClip, timestamps : Sel.timeStamps, precis
     for times in timestamps.time_list:
         list_of_vid.append(cutVideo(clip,times[0],times[1]))
     
-    #security checks: what if list gives thing out of order
+    #security checks: what if list gives thing out of order and others
     return list_of_vid
         
+        
+
 
 
 
@@ -43,10 +55,16 @@ def cutTimeStampClips(clip:mp.VideoFileClip, timestamps : Sel.timeStamps, precis
 if __name__ == "__main__":
     
     timestamps = Sel.timeStamps("Test_Folder/test.txt")
-    
-    print("Testing: video_manipulations.py ;")
     temp = cutVideo(mergeClips("Test_Folder/"),35,40)
-    exportClip(temp,"Test_Folder/","h.mp4",(1000,800))
+    #exportClip(temp,"Test_Folder/","h.mp4",(1000,800))
+    
+    temp2 = cutVideo(mergeClips("Test_Folder/"),60,69)
+    
+    l = []
+    l.append(temp)
+    l.append(temp2)
+    
+    exportClipList(l,"Test_Folder/","export_folder")
     
     
     print("Finished testing Video_Manipulations.py")
